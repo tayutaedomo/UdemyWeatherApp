dee82b6bc3fdd9b73b7171ae6591988f3f409f2d
tayutaedomo <tayutaedomo@gmail.com>
2017-06-22 19:46:57 +0900

    Add web service API call

--- app/src/main/java/net/tayutaedomo/udemyweatherapp/MainActivity.java
+++ app/src/main/java/net/tayutaedomo/udemyweatherapp/MainActivity.java
@@ -2,12 +2,48 @@ package net.tayutaedomo.udemyweatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=140010";
+
+        JsonObjectRequest jsObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d(TAG, response.toString(2));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, error.toString());
+                    }
+                }
+        );
+
+        MySingleton.getInstance(this).addToRequestQueue(jsObjectRequest);
     }
 }
