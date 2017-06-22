0ea5bee8ea7d44130a172fd0cfa5873590ca2943
tayutaedomo <tayutaedomo@gmail.com>
2017-06-22 20:47:51 +0900

    Implement data binding

--- app/src/main/java/net/tayutaedomo/udemyweatherapp/MainActivity.java
+++ app/src/main/java/net/tayutaedomo/udemyweatherapp/MainActivity.java
@@ -3,6 +3,7 @@ package net.tayutaedomo.udemyweatherapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -15,10 +16,22 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    TextView mTitle;
+    TextView mDescription;
+    TextView mDateLabel0, mDateLabel1;
+    TextView mTelop0, mTelop1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTitle = (TextView)findViewById(R.id.title);
+        mDescription = (TextView)findViewById(R.id.description);
+        mDateLabel0 = (TextView)findViewById(R.id.dateLabel0);
+        mDateLabel1 = (TextView)findViewById(R.id.dateLabel1);
+        mTelop0 = (TextView)findViewById(R.id.telop0);
+        mTelop1 = (TextView)findViewById(R.id.telop1);
 
         String url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=140010";
 
@@ -31,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         try {
                             Log.d(TAG, response.toString(2));
+
+                            mTitle.setText(response.getString("title"));
+                            mDescription.setText(response.getJSONObject("description").getString("text"));
+                            mDateLabel0.setText(response.getJSONArray("forecasts").getJSONObject(0).getString("dateLabel"));
+                            mDateLabel1.setText(response.getJSONArray("forecasts").getJSONObject(1).getString("dateLabel"));
+                            mTelop0.setText(response.getJSONArray("forecasts").getJSONObject(0).getString("telop"));
+                            mTelop1.setText(response.getJSONArray("forecasts").getJSONObject(1).getString("telop"));
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
