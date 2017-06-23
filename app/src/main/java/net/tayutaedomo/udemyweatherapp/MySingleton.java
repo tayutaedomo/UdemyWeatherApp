988aa7e611d1befdce68c94aa38bdfaf6dbfc1d8
tayutaedomo <tayutaedomo@gmail.com>
2017-06-23 18:40:06 +0900

    Add ImageLoader

--- app/src/main/java/net/tayutaedomo/udemyweatherapp/MySingleton.java
+++ app/src/main/java/net/tayutaedomo/udemyweatherapp/MySingleton.java
@@ -1,9 +1,12 @@
 package net.tayutaedomo.udemyweatherapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 /**
@@ -14,6 +17,7 @@ class MySingleton {
     private static MySingleton ourInstance;
     private RequestQueue mRequestQueue;
     private static Context mContext;
+    private ImageLoader mImageLoader;
 
     public static synchronized MySingleton getInstance(Context context) {
         if (ourInstance == null) {
@@ -25,6 +29,20 @@ class MySingleton {
     private MySingleton(Context context) {
         mContext = context;
         mRequestQueue = getRequestQueue();
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
     }
 
     public RequestQueue getRequestQueue() {
@@ -37,6 +55,10 @@ class MySingleton {
 
     public <T> void addToRequestQueue(Request<T> req) {
         getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImmageLoader() {
+        return mImageLoader;
     }
 }
 
